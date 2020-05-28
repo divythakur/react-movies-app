514a1baaaabe4f84627197c081dfdd42d7db4b1b
divythakur <divyanshuthakur75@gmail.com>
2020-05-28 17:23:25 +0530

    added modal in login

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,23 +1,41 @@
 import React,{Component} from 'react';
 import './Header.css'
+import Modal from 'react-modal'
 
 import Button from '@material-ui/core/Button';
 
 
 
-function Header(){
-    
+class Header extends Component{
+    constructor(){
+        super();
+        this.state={
+            modalIsOpen:false
+
+        };
+    }
+    openModalHandler= ()=>{
+        this.setState({modalIsOpen:true})
+    }
+    closeModelHandler= () =>{
+        this.setState({modalIsOpen:false})
+    }
+    render(){
         return(
             <div>
                 {/* <Button variant="contained" color="default">Login</Button> */}
                 <div className="header-com">
                     <img  className="app-logo" src={"https://cdn.upgrad.com/UpGrad/temp/81930fe3-8dc5-47d1-b7f9-d00847a4f057/logo.svg"} alt="s" />
-                <button style={{float:"right",marginRight:"15px",marginTop:"8px"}}>Login</button></div>
-                
+                <button style={{float:"right",marginRight:"15px",marginTop:"8px"}} onClick={this.openModalHandler}>Login</button></div>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login"
+                onRequestClose={this.closeModelHandler}>
+
+                </Modal>
                 
             </div>
 
         )
+    }
     
 }
 export default Header;
